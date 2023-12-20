464cb67718272068d532dcd257b08b5a9e3db415
kevinjeremia <jungzstrumida@gmail.com>
2023-12-20 13:32:53 +0700

    Arrays and Strings exercises

---
+++ array-and-string/learning.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    char alphabet[3] = {'a','b','c'};
+    printf("Tell me the first 3 alphabet please: %c %c %c\n", alphabet[0], alphabet[1], alphabet[2]);
+    
+    int num[3] = {1,2,3};
+    printf("this is one: %d\n", num[0]);
+    printf("this is two: %d\n", num[1]);
+    printf("this is three: %d\n", num[2]);
+    char string_attempt[6] = "alicia";
+
+    printf("Hi, my name is %s\n", string_attempt);
+
+    string_attempt[5] = 'o';
+    printf("Now, my name is %s\n", string_attempt);
+    
+    char *another = "alicio";
+
+    printf("Hi, this is my another name %s\n", another);
+    char *stratt = &string_attempt[5];
+    printf("Value of pointer another %p\n", (void *)another);
+    printf("Value of pointer stratt %p\n", (void *)stratt);
+    
+    return 0;
+}
