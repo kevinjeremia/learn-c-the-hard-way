39dcc293554995b06fe6d1cef3fe1e5efcd17d53
kevinjeremia <jungzstrumida@gmail.com>
2023-12-20 11:07:36 +0700

    clean the repo and add makefile to each directory

---
+++ exercise-3/ex3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/* Formatted Printing */
+int main()
+{
+    int age = 20;
+    int height = 163;
+
+    printf("I am %d years old.\n", age);
+    printf("I am %d centimeters tall.\n", height);
+}
