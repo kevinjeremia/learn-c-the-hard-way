d3dc8c2bd01d2bdaee2297d4e196c8ca5760b575
kevinjeremia <jungzstrumida@gmail.com>
2023-12-20 08:33:51 +0700

    first exercise

---
+++ dust-off-that-compiler.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/* The first exercise is basically an intro to C*/
+int main(int argc, char *argv[]) {
+    int distance = 100;
+    printf("I've been running for %d meters.\n", distance);
+    char name[5] = "Kevin";
+    printf("Hi my name is %s", name);
+    return 0;
+}
